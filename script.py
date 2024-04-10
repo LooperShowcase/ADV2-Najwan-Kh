f64d242bb30068bcbdf0243eefe6a701769b640a
Looper Project Showcases <87170787+LooperShowcase@users.noreply.github.com>
2024-04-10 13:27:33 +0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,9 @@
+def my_sum(n1,n2,n3,n4,n5,n6):
+    return n1+n2+n3+n4+n5+n6
+
+    def my_avg(x,y,z,e,w,f):
+        return (x+y+z+e+w,f)/5
+        print("The average is" , my_avg(10,20,30,40,50,60))
+
+
+    
